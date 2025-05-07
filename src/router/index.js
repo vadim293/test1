a69e3a8a343038d530e5ca4f2a42fc96b0f55e24
vadim293 <Vadimbob2107@mail.ru>
2025-05-07 19:09:34 +0600

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '../views/HomePage.vue';
+import RegisterPage from '../views/RegisterPage.vue';
+import LoginPage from '../views/LoginPage.vue';
+
+const routes = [
+  { path: '/', component: HomePage },
+  { path: '/register', component: RegisterPage },
+  { path: '/login', component: LoginPage },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(to) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth',
+        top: 100, // Отступ сверху (в пикселях)
+      };
+    }
+    return { top: 0 };
+  }  
+});
+
+export default router;
 
