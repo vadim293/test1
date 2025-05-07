c8f44bf67ce7f17176c3c4c71201c1b1fb2b1d09
vadim293 <Vadimbob2107@mail.ru>
2025-05-07 19:05:21 +0600

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router'; // Импортируйте router
+import '../src/style/index.css'; 
 
+const app = createApp(App);
+app.use(router); // Используйте router
+
+app.mount('#app'); // Монтируйте приложение
