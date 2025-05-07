59d0c7ebe6deb3d527afaf4eae2299618dc35f3f
vadim293 <Vadimbob2107@mail.ru>
2025-05-07 19:08:27 +0600

    Add files via upload

--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,15 @@
+import { reactive } from 'vue';
 
+const store = reactive({
+  isAuthenticated: !!localStorage.getItem('token'),
+  token: localStorage.getItem('token'),
+  logout: () => {
+    if(store.token) {
+      store.token = null;
+    }
+    store.isAuthenticated = false;
+    localStorage.removeItem('token');
+  },
+});
+
+export default store;
