bff5eb95af916d217d42147d6bddd276d74743b3
vadim293 <Vadimbob2107@mail.ru>
2025-05-08 23:09:23 +0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    // Разрешить все хосты (для разработки)
+    allowedHosts: 'all',
+    
+    // Или можно указать конкретные хосты:
+    // allowedHosts: ['localhost', 'myapp.test', '.mydomain.com'],
+    
+    // Дополнительные настройки для избежания проблем с HMR
+    hot: true,
+    client: {
+      webSocketURL: 'auto://0.0.0.0:0/ws'
+    }
+  }
 })
