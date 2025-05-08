681b67a2c358fa767c00a5130adbe926b27188f9
vadim293 <Vadimbob2107@mail.ru>
2025-05-08 23:16:33 +0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true, 
+  devServer: {
+    disableHostCheck: true, // Отключает проверку заголовка Host
+    port: 8080, // Убедитесь, что порт соответствует тому, который используется на Render
+  }, 
 })
