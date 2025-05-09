a1c9c6298d1f1ce016f496b13dcd58f1e514f6ea
vadim293 <Vadimbob2107@mail.ru>
2025-05-09 12:58:54 +0600

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 10000,
-    allowedHosts: 'all',
-    client: {
-      webSocketURL: {
-        hostname: '0.0.0.0',
-        port: process.env.PORT || 10000,
-        pathname: '/'
-      }
-    },
-    historyApiFallback: true,
-    hot: true,
-    compress: true
-  }
+  transpileDependencies: true
 })
