c752a1b2da2ce1a338133e15202788298fde8399
vadim293 <Vadimbob2107@mail.ru>
2025-05-09 11:16:01 +0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-  allowedHosts: [
-    'localhost',
-    'example.com',
-    '.example.com' // поддомены
-  ]
-  } 
-}) 
+    allowedHosts: "all",
+    host: '0.0.0.0',  // Разрешает доступ с любого IP (нужно для Render.com)
+    port: 10000,      // Использует порт, назначенный Render
+    client: {
+      webSocketURL: 'auto://0.0.0.0:10000/ws'  // Важно для HMR (Hot Module Replacement)
+    }
+  }
+})
